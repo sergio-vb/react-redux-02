24177a50451259ee97f5082010feb1da6d1fde99
svb <sergio.valverde.b@gmail.com>
2017-05-11 20:06:24 -0600

    Using middleware to handle async actions

--- src/index.js
+++ src/index.js
@@ -29,9 +29,13 @@ store.dispatch({type: "INCREMENT", payload: 70});*/
 
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from "redux-logger";
+import thunk from "redux-thunk";
+import axios from "axios";
+//import promise from "redux-promise-middleware";
 
 const customLogger = (store) => (next) => (action) => {
 	console.log("Middleware logged an action");
+	//action.type = "DEC"; <-We could forcibly change the type for all incoming actions
 	next(action);
 }
 //Could use a custom middleware to catch errors:
@@ -43,7 +47,7 @@ const error = (store) => (next) => (action) => {
 	}
 }
 
-const middleware = applyMiddleware(logger);
+const middleware = applyMiddleware(thunk, logger);
 
 const userReducer = (state={}, action) => {
 
@@ -55,20 +59,50 @@ const userReducer = (state={}, action) => {
 			state = Object.assign({}, state, {age: action.payload});
 			break;
 	}
-	return state;
+	return Object.assign({}, state);
 }
+
 const tweetsReducer = (state=[], action) => {
 
 	switch (action.type){
 		case "ADD_TWEET":
 			state = state.concat(action.payload);
 	}
-	return state;	
+	return state.concat();
+}
+
+
+let usersLogInitialState = {
+	fetching: false,
+	fetched: false,
+	users: [],
+	error: null
+}
+const usersLogReducer = (state=usersLogInitialState, action) => {
+
+	switch (action.type){
+		case "FETCH_USERS_START":
+			return Object.assign({}, state, {fetching: true});
+			break;
+		case "FETCH_USERS_ERROR":
+			return Object.assign({}, state, {error: action.payload});
+			break;
+		case "RECEIVE_USERS":
+			return Object.assign({}, state, {
+				fetching: false,
+				fetched: true,
+				users: action.payload,
+				error: null
+			});
+			break;
+	}
+	return Object.assign({}, state);
 }
 
 const reducers = combineReducers({
 	user: userReducer,
-	tweets: tweetsReducer
+	tweets: tweetsReducer,
+	usersLog: usersLogReducer
 });
 const store = createStore(reducers, {}, middleware);
 
@@ -79,3 +113,29 @@ store.subscribe( () => {
 store.dispatch({type: "CHANGE_NAME", payload: "Kell"});
 store.dispatch({type: "CHANGE_AGE", payload: 27});
 store.dispatch({type: "ADD_TWEET", payload: "Random Tweet"});
+
+store.dispatch( (dispatch) => {
+	dispatch({type: "FETCH_USERS_START"});
+	axios.get("http://rest.learncode.academy/api/wstern/users")
+		.then( (response) => {
+			dispatch({type: "RECEIVE_USERS", payload: response.data});
+		})
+		.catch( (err) => {
+			dispatch({type: "FETCH_USERS_ERROR", payload: err});
+		});
+
+});
+
+/* Using redux-promise-middleware is a bit cleaner:
+
+store.dispatch({
+	type: "EXAMPLE_ACTION",
+	payload: axios.get("http://rest.learncode.academy/api/wstern/users")
+});
+
+The middleware will detect that the payload is of a promise type and then
+automatically dispatch actions of type X_PENDING, X_FULFILLED, or X_REJECTED as needed
+
+
+source: https://www.youtube.com/watch?v=Td-2D-_7Y2E
+*/
